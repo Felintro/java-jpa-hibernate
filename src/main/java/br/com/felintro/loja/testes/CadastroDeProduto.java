66fda62ab270af23bee418381e0e556e4dcdff7c
Allan Chavier Felintro <allan.felintro@matera.com>
2023-02-11 11:31:53 -0300

    Remoção de imports redundantes

--- src/main/java/br/com/felintro/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/felintro/loja/testes/CadastroDeProduto.java
@@ -11,8 +11,6 @@ import br.com.felintro.loja.model.Produto;
 import br.com.felintro.loja.utils.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
