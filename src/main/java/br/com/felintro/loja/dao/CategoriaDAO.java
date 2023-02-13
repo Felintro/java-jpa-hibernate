70fad2f988d03acb527916c1c9ac7f4ce70d4718
Allan Chavier Felintro <allan.felintro@matera.com>
2023-02-13 17:32:26 -0300

    Implementação de métodos de busca para Categoria

--- src/main/java/br/com/felintro/loja/dao/CategoriaDAO.java
+++ src/main/java/br/com/felintro/loja/dao/CategoriaDAO.java
@@ -10,6 +10,7 @@ import br.com.felintro.loja.model.Categoria;
 import lombok.AllArgsConstructor;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @AllArgsConstructor
 public class CategoriaDAO {
@@ -27,6 +28,30 @@ public class CategoriaDAO {
     public void remover(Categoria categoria) {
         categoria = this.entityManager.merge(categoria);
         this.entityManager.remove(categoria);
+    }
+
+    public Categoria buscarPorId(Long idCategoria) {
+        String queryJPQL = "SELECT c FROM Categoria c WHERE c.id = :idCategoria";
+        return this.entityManager
+            .createQuery(queryJPQL, Categoria.class)
+            .setParameter("idCategoria", idCategoria)
+            .getSingleResult();
+    }
+
+    /* Nesta aplicação, cada categoria possui um nome único! */
+    public Categoria buscarPorNome(String nomeCategoria) {
+        String queryJPQL = "SELECT c FROM Categoria c WHERE c.nome = :nomeCategoria";
+        return this.entityManager
+            .createQuery(queryJPQL, Categoria.class)
+            .setParameter("nomeCategoria", nomeCategoria)
+            .getSingleResult();
+    }
+
+    public List<Categoria> buscarTodos() {
+        String queryJPQL = "SELECT c FROM Categoria c";
+        return this.entityManager
+            .createQuery(queryJPQL, Categoria.class)
+            .getResultList();
     }
 
 }
